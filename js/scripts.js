bad1a00a1d30b6f34ee71ddb1852fa0728d38936
Nicholas Gunner <support@getbeyondwords.com>
2018-05-08 22:15:37 -0600

    360 up and running

--- js/scripts.js
+++ js/scripts.js
@@ -19,68 +19,43 @@ map.on('load', function () {
     }
   });
 
-  map.addLayer({
-    "id": "Panoramas",
-    "type": "symbol",
-    "source": {
-        "type": "geojson",
-        "data": {
-            "type": "FeatureCollection",
-            "features": [
-              {
-                "type": "Feature",
-                "properties": {
-                    "content": "<iframe allowfullscreen='true' scrolling='no' src='panoramas/mud_lake.html'></iframe>",
-                    "icon": "theatre"
-                },
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [-79.316538, 42.354815]
-                }
-              }
-            ]
-        }
-    },
-    "layout": {
-        "icon-image": "{icon}-15",
-        "icon-allow-overlap": true
-    }
-  });
+  var geojson = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "properties": {
+              "content": "<iframe allowfullscreen='true' scrolling='no' src='panoramas/mud_lake.html' style='border:0;'></iframe>"
+          },
+          "geometry": {
+              "type": "Point",
+              "coordinates": [-79.316538, 42.354815]
+          }
+        }]
+      };
 
-  // When a click event occurs on a feature in the places layer, open a popup at the
-  // location of the feature, with description HTML from its properties.
-  map.on('click', 'Panoramas', function (e) {
-      var coordinates = e.features[0].geometry.coordinates.slice();
-      var content = e.features[0].properties.content;
 
-      // Ensure that if the map is zoomed out such that multiple
-      // copies of the feature are visible, the popup appears
-      // over the copy being pointed to.
-      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-      }
 
-      new mapboxgl.Popup()
-          .setLngLat(coordinates)
-          .setHTML(content)
-          .addTo(map);
-  });
+  // add markers to map
+  geojson.features.forEach(function(marker) {
 
-  // Change the cursor to a pointer when the mouse is over the places layer.
-  map.on('mouseenter', 'Panoramas', function () {
-      map.getCanvas().style.cursor = 'pointer';
-  });
+    // create a HTML element for each feature
+    var el = document.createElement('div');
+    el.className = 'marker';
 
-  // Change it back to a pointer when it leaves.
-  map.on('mouseleave', 'Panoramas', function () {
-      map.getCanvas().style.cursor = '';
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+    .setLngLat(marker.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    .setHTML(marker.properties.content))
+    .addTo(map);
   });
 
 });
 
 
 // Contol the menu
-var toggleableLayerIds = [ 'Illustration', 'Panoramas' ];
+var toggleableLayerIds = [ 'Illustration' ];
 
 for (var i = 0; i < toggleableLayerIds.length; i++) {
     var id = toggleableLayerIds[i];
